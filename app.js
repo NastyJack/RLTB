a107e3ea2a62fd1debadf0bb4852724ccbbc8d3c
NastyJack <cross99.rishabh@gmail.com>
2023-04-17 13:45:15 +0530

    added run on app start

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config({ path: "credentials.env" });
 const express = require("express");
 const app = express();
 const tradeBumper = require("./routes");
-const fs = require("fs");
+const axios = require("axios").default;
 
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +17,16 @@ app.listen(process.env.PORT, async () => {
   console.log(
     `\n RLTB is listening at port ${process.env.PORT} @ ${process.env.NODE_ENV}`
   );
+
+  makeRequest();
 });
+
+makeRequest = async () => {
+  try {
+    await axios.post("http://localhost:8080/api/tradeBumper/start", {});
+  } catch (e) {
+    console.log("Error at makeRequest", e);
+  }
+};
 
 module.exports = app;
